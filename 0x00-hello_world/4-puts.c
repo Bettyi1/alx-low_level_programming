37331d95307daa271b02a382d3e6cc1a6fd344e6
Beatrice Irungu <bettyirungu15@gmail.com>
2023-03-16 07:01:44 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - this is the main function
- *
- * Return: 0
+ * main - entry point
+ * description - this is the main function
+  * Return:always 0
  */
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
