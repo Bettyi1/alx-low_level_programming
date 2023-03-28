9668217b130f43220a5846e1f52444af57aa089e
Beatrice Irungu <bettyirungu15@gmail.com>
2023-03-28 01:46:27 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - function should print only one character out of two
+ * starting with the first one
+ * @str: input
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
