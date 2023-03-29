2c311731922b083f4c2ea66a30e288f6de29963f
Beatrice Irungu <bettyirungu15@gmail.com>
2023-03-29 08:39:34 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - function copies string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of elements
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i] <= '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
