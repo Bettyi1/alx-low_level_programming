b0eaca887c36d1ebb7caea318463d0008ba78747
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-12 06:07:25 -0700

    concatenates

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
- * argstostr -concatenates arguments of a program
+ * argstostr - main function
  * @ac: input value
  * @av:  pointer array
  * Return: 0
@@ -14,19 +13,16 @@ char *argstostr(int ac, char **av)
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
 	for (a = 0; a < ac; a++)
 	{
 		for (b = 0; av[a][b] != '\0'; b++)
 			len++;
 		len++;
 	}
-
 	str = malloc(sizeof(char) * (len + 1));
 
 	if (str == NULL)
 		return (NULL);
-
 	k = 0;
 
 	for (a = 0; a < ac; a++)
@@ -39,6 +35,5 @@ char *argstostr(int ac, char **av)
 		str[k] = '\n';
 		k++;
 	}
-
 	return (str);
 }
