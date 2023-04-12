b158dc8db9fe441466972aea988414e25377351d
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-12 01:36:43 -0700

    function that returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - function copies string as per parameter
+ * @str: string
+ *
+ * Return: pointer to the copied string (Success), NULL (Error)
+ */
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int k, len;
+
+	k = 0;
+	len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+	duplicate = malloc(sizeof(char) * (len + 1));
+
+	if (duplicate == NULL)
+		return (NULL);
+
+	while ((duplicate[k] = str[k]) != '\0')
+		k++;
+	return (duplicate);
+}
