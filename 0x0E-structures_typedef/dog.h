81c8ed0c8cf82f71a8da0be1a96443027281846e
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-17 05:42:47 -0700

    new structure with elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define STRUCT_DOG_H
 /**
  * struct dog -contains the dog elements
- *
  * @name: input info
  * @age: input info
  * @owner: input info
+ * Description: long description
  */
 struct dog
 {
@@ -14,8 +14,7 @@ struct dog
 	char *owner;
 };
 /* other prototypes*/
-
-dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
