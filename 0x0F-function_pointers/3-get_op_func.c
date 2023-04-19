a42492e9d5082a71e89694d6b68c76464dd98fe6
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-19 12:00:44 -0700

    arithmetic operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,24 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_op_func - selects the correction function
+ * @s:argument
+ * Return: pointer to funtion
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+	int k = 0;
+	
+	while (ops[k].op != NULL && *(ops[k].op) != *s)
+		k++;
+	return (ops[k].f);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ * op_add-functions returns sum
+ * @a: input value
+ * @b: input value
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	int sum = a+b;
+	return (sum);
+}
+
+/**
+ * op_sub - function returns difference btwn two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: The difference of a and b.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - product of two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: product of a and b.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_mod - reminder of division btwn two numbers.
+ * @a: input value
+ * @b: input value
+ * Return: result
+ *
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
+/**
+ * op_div - division of two numbers.
+ * @a: input value
+ * @b: input value
+  * Return: result
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
