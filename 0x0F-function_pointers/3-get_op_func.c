84502e229136fb5dc389b4108c4c676246d47994
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-19 12:30:05 -0700

    multiple, simple arithmetics

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,7 +17,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL},
 	};
 	int k = 0;
-	
+
 	while (ops[k].op != NULL && *(ops[k].op) != *s)
 		k++;
 	return (ops[k].f);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
-
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-
 /**
  * op_add-functions returns sum
  * @a: input value
@@ -16,7 +9,8 @@ int op_mod(int a, int b);
  */
 int op_add(int a, int b)
 {
-	int sum = a+b;
+	int sum = a + b;
+
 	return (sum);
 }
 
