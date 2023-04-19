b19a160ff012d3792f44e695a86c2ac4c338764a
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-19 12:24:58 -0700

    multiple functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * op_add-functions returns sum
  * @a: input value
