1f69330f44276f469454c2e520c541cd8ce0b289
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-21 11:14:26 -0700

    a function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,6 +7,7 @@
  * @n: number of arguments
  * @separator: separator
  * @...:last parameter
+ * Return: 0
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
@@ -36,6 +37,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		}
 		printf("%s", separator);
 	}
-	va_end(string);
 	printf("\n");
+	va_end(string);
 }
