708f2814b05284141d40c8ec6736055217dc93f3
Beatrice Irungu <bettyirungu15@gmail.com>
2023-04-28 11:06:37 -0700

    a function that prints text

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+void func(void) __attribute__((constructor)), before_main();
 /**
- * first - function prints text before the main
+ * func - function prints text before the main
  * descrip - exeecut3 function
+ *
  */
-void __attribute__((constructor)) before_main()
+void func(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
