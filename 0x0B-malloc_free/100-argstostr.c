6c4f405123e00a300357941cdcf749ca26782abb
Beatrice Irungu <bettyirungu15@gmail.com>
2023-05-02 12:19:10 -0700

    a function that concatenates all the arguments of your program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * argstostr - main function
  * @ac: input value
@@ -8,32 +9,31 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int a, b, k, len;
+	int a, b, k = 0, x = 0;
 	char *str;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-	for (a = 0; a < ac; a++)
+	for (a = 0 ; a < ac ; a++)
 	{
-		for (b = 0; av[a][b] != '\0'; b++)
-			len++;
-		len++;
+		for (b = 0 ; av[a][b] ; b++)
+			x++;
 	}
-	str = malloc(sizeof(char) * (len + 1));
-
+	x += ac;
+	str = malloc(sizeof(char) * x + 1);
 	if (str == NULL)
 		return (NULL);
-	k = 0;
-
-	for (a = 0; a < ac; a++)
+	for (a = 0 ; a < ac ; a++)
 	{
-		for (b = 0; av[a][b] != '\0'; b++)
+		for (b = 0 ; av[a][b] ; b++)
 		{
 			str[k] = av[a][b];
 			k++;
 		}
-		str[k] = '\n';
-		k++;
+		if (str[k] == '\0')
+		{
+			str[k++] = '\n';
+		}
 	}
 	return (str);
 }
