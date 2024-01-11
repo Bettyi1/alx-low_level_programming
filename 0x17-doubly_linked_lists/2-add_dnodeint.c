fffa997c3de940746ea1190130406c1bf2c0df1f
betty irungu <bettyirungu15@gmail.com>
2024-01-11 12:29:34 -0800

    adds a new node at the beginning of alist

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - func adds new node to list  beginning
+ * @head: Pointer
+ * @n: Integer
+ *
+ * Return: address or NULl
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+		return (*head);
+}
