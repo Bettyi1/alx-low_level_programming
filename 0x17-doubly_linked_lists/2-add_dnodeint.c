6a0ef4a1d21cf205a6b8b981ac9bae6cdfba0147
betty irungu <bettyirungu15@gmail.com>
2024-01-11 12:34:05 -0800

    adds new node to a list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,11 +11,11 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-
+	
 	if (!new)
 		return (NULL);
 	new->n = n;
 	new->next = *head;
 	*head = new;
-		return (*head);
+	return (*head);
 }
